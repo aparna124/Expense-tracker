671f6fc66ef553404b1f0267adbee3b2176747a0
aparna124 <41099165+aparna124@users.noreply.github.com>
2022-01-06 15:23:53 -0500

    Added click event and state change

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
@@ -7,9 +7,18 @@ import './ExpenseItem.css';
 const ExpenseItem = (props) =>
 {
 
-    const month = props.date.toLocaleString('en-US', {month: 'long'});
-    const day = props.date.toLocaleString('en-US', {day: '2-digit'});
-    const year = props.date.getFullYear();
+    // const month = props.date.toLocaleString('en-US', {month: 'long'});
+    // const day = props.date.toLocaleString('en-US', {day: '2-digit'});
+    // const year = props.date.getFullYear();
+
+    const [title, setTitle]  = useState(props.title);
+    const [amount, setAmount] = useState(props.amount);
+
+    const clickHandler = () =>
+    {
+        setTitle("Updated");
+        setAmount(100);
+    } 
 
     return (
         <Card className="expense-item">
@@ -17,9 +26,11 @@ const ExpenseItem = (props) =>
             
 
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
-                <div className="expense-item__price">{props.amount}</div>
+                <h2>{title}</h2>
+                <div className="expense-item__price">{amount}</div>
             </div>
+
+            <button onClick={clickHandler}>Clicked!</button>
 
         </Card>
         
