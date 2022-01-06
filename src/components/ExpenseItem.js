6521cba824d536fc23c3b6250f12e362674eda70
aparna124 <41099165+aparna124@users.noreply.github.com>
2022-01-05 20:26:07 -0500

    Basic UI done

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,23 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
-function ExpenseItem()
+function ExpenseItem(props)
 {
 
-    const expenseDate = new Date(2021, 2, 28);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 294.67;
+    const month = props.date.toLocaleString('en-US', {month: 'long'});
+    const day = props.date.toLocaleString('en-US', {day: '2-digit'});
+    const year = props.date.getFullYear();
 
     return (
         <div className="expense-item">
-            <div>March 28th 2021</div>
+            <ExpenseDate date={props.date} />
+            
+
             <div className="expense-item__description">
-                <h2>Car Insurance</h2>
-                <div className="expense-item__price">$294.67</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">{props.amount}</div>
             </div>
+
         </div>
         
 
