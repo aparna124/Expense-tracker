625b21309b2a3bc078118a3e07d8815865c64e52
aparna124 <41099165+aparna124@users.noreply.github.com>
2022-01-07 14:11:24 -0500

    Added method to clear input data

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -54,6 +54,9 @@ const submitHandler = (event) =>
     };
 
     console.log(expenseData);
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
 };
 
   return (
@@ -61,21 +64,21 @@ const submitHandler = (event) =>
 
         <div className="new-expense__control">
             <label>Title</label>
-            <input type="text" onChange={titleChangeHandler}/>
+            <input type='text' value={enteredTitle} onChange={titleChangeHandler}/>
         </div>
 
         <div className="new-expense__control">
             <label>Amount</label>
-            <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>
+            <input type='number' min="0.01" step="0.01" value={enteredAmount} onChange={amountChangeHandler}/>
         </div>
 
         <div className="new-expense__control">
             <label>Date</label>
-            <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler}/>
+            <input type='date' min="2019-01-01" max="2022-12-31" value={enteredDate} onChange={dateChangeHandler}/>
         </div>
 
         <div className="new-expense__actions">
-            <button type="submit">Add Expense</button>
+            <button type='submit'>Add Expense</button>
         </div>
 
       
