829d0c1274c232a75773e2730c8e0886930d8c9c
aparna124 <41099165+aparna124@users.noreply.github.com>
2022-01-11 13:46:15 -0500

    Added condition for displaying expenses

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -19,28 +19,34 @@ const Expenses = (props) =>
     const filteredExpenses = props.items.filter(expense =>
       {
         return expense.date.getFullYear().toString() === filteredYear;
-      })
+      });
 
-    return (
-    <div>
-      <Card className="expenses">
-        <ExpensesFilter
-          selected={filteredYear}
-          onChangeFilter={filterChangeHandler}
-        />
-      
-        {filteredExpenses.map((expense) => (
+
+    let expensesContent = <p> No items find </p>
+
+    if(filteredExpenses.length > 0)
+    {
+      expensesContent = filteredExpenses.length > 0 && (
+        filteredExpenses.map((expense) => (
           <ExpenseItem
             key={expense.id}
             title={expense.title}
             amount={expense.amount}
             date={expense.date}
           />
-        ))}
+        )
+      ))
+    }
 
-
-      </Card>
-    </div>
+    return (
+      <div>
+        <Card className="expenses">
+          <ExpensesFilter
+            selected={filteredYear}
+            onChangeFilter={filterChangeHandler}
+          />
+        </Card>
+      </div>
     );
 }
 
